5929f5ef360db490dc538ff0d430f87c48e63479
Eagle <Eagle@10.96.198.193>
2017-10-08 22:10:40 +0800

    Update

--- src/main/java/devutility/test/service/App.java
+++ src/main/java/devutility/test/service/App.java
@@ -4,10 +4,15 @@ package devutility.test.service;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		for (int i = 0; i < 30; i++) {
+			try {
+				Thread.sleep(3000);
+				System.out.println("Hello World!");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
